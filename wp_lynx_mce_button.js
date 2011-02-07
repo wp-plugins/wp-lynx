fe8a6b30a673deb277b66d71971311296a9a4179
mtekk <mtekk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-07 15:23:21 +0000

    removing cruft from mce button code
    git-svn-id: https://plugins.svn.wordpress.org/wp-lynx/trunk@341953 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_lynx_mce_button.js
+++ wp_lynx_mce_button.js
@@ -10,14 +10,6 @@
 					tinymce.DOM.setStyle( ['TB_overlay','TB_window','TB_load'], 'z-index', '999999' );
 				}
 			});
-			// Add Media buttons to fullscreen and handle align buttons for image captions
-			ed.onBeforeExecCommand.add(function(ed, cmd, ui, val, o) {
-				var DOM = tinymce.DOM, n, DL, DIV, cls, a, align;
-				if ( 'mceFullScreen' == cmd ) {
-					if ( 'mce_fullscreen' != ed.id && DOM.get('add_link_print'))
-						ed.settings.theme_advanced_buttons1 += ',add_link_print';
-				}
-			});
 			ed.onInit.add(function(ed) {
 				ed.plugins.add_link_print._hideButtons();
 			});
@@ -25,28 +17,6 @@
 				ed.plugins.add_link_print._hideButtons();
 			});
 		},
-		_showButtons : function(n, id) {
-			var ed = tinyMCE.activeEditor, p1, p2, vp, DOM = tinymce.DOM, X, Y;
-
-			vp = ed.dom.getViewPort(ed.getWin());
-			p1 = DOM.getPos(ed.getContentAreaContainer());
-			p2 = ed.dom.getPos(n);
-
-			X = Math.max(p2.x - vp.x, 0) + p1.x;
-			Y = Math.max(p2.y - vp.y, 0) + p1.y;
-
-			DOM.setStyles(id, {
-				'top' : Y+5+'px',
-				'left' : X+5+'px',
-				'display' : 'block'
-			});
-
-			if ( this.mceTout )
-				clearTimeout(this.mceTout);
-
-			this.mceTout = setTimeout( function(){ed.plugins.add_link_print._hideButtons();}, 5000 );
-		},
-
 		_hideButtons : function() {
 			/*if ( !this.mceTout )
 				return;*/
